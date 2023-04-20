6025fc52037f6f3282d0ec8dcf989c1206d7330c
Manunyville <125371151+Manunyville@users.noreply.github.com>
2023-04-20 19:49:55 -0300

    incorpore los testimonios dentro del componente

--- src/componentes/servicio/servicio.jsx
+++ src/componentes/servicio/servicio.jsx
@@ -7,6 +7,13 @@ import diseño from '../../assets/img/Diseñodepagina.png'
 import Diseñoweb from '../../assets/img/desarrolloweb.png'
 import Fotaki from '../../assets/img/codigoweb.png'
 import Azderecha from '../../assets/img/ImagenazulD.png'
+import Five from '../../assets/img/fivestars.png'
+import Comillas from '../../assets/img/comillas.png'
+import Azul from '../../assets/img/imgabstractaizquierda.png'
+import { FaUserLock } from 'react-icons/fa'
+import marcos from '../../assets/img/Marcos.jpg'
+import alexis from '../../assets/img/alexis.jpg'
+import cami from '../../assets/img/Camila.jpeg'
 
 const Service = () => {
 
@@ -50,41 +57,45 @@ const Service = () => {
                     </HStack>
                 </VStack>
             </Container>
-            <Container width='1200px' id='proyectos'>
+
+
+            <Container width='full' id='proyectos'>
                 <VStack >
                     <VStack>
-                        <Stack >
-                            <Image src={Azderecha} className='imgAbs' marginLeft="800px" />
-                        </Stack>
+                        <HStack >
+                            <Image src={Azderecha} className='imgAbs' />
+                        </HStack>
                         <Text color={'#0C7FB0'}>Recent projects</Text>
                         <Heading>My Portfolio</Heading>
                     </VStack>
-                    <HStack gap='75px'>
-                        <Card width='270px' height='350px' bg={'#F9F9F9'} marginLeft='10px'>
-                            <CardHeader>
+                    <HStack gap='75px' className='stackPortfolio'>
+                        <Card width='270px' height='350px' bg={'#F9F9F9'} marginLeft='10px' className='cardsProyecto'>
+                            <Stack>
                                 <Image src={Fotaki} height='150px' />
                                 <Heading size='md'> Portfolio proyecto</Heading>
-                            </CardHeader>
+                            </Stack>
                             <CardBody>
                                 <Text>Es una version anterior del Portfolio actual</Text>
                                 <Button marginTop='4px' as={motion.button} paddingX="65px" whileHover={{ color: '#0C7FB0' }} color={'#F9F9F9'} bg={'#0C7FB0'} variant='solid'>View on github</Button>
                             </CardBody>
                         </Card>
-                        <Card width='270px' height='350px' bg={'#F9F9F9'} marginLeft='10px'>
-                            <CardHeader>
+                        <Card width='270px' height='350px' bg={'#F9F9F9'} marginLeft='10px' className='cardsProyecto'>
+                            <Stack>
                                 <Image src={Fotaki} height='150px' />
                                 <Heading size='md'> Forniture web</Heading>
-                            </CardHeader>
+                            </Stack>
                             <CardBody>
                                 <Text>I create a web page for a forniture market</Text>
                                 <Button marginTop='4px' as={motion.button} paddingX='65px' whileHover={{ color: '#0C7FB0' }} color={'#F9F9F9'} bg={'#0C7FB0'} variant='solid'>View on github</Button>
                             </CardBody>
                         </Card>
-                        <Card width='270px' height='350px' bg={'#F9F9F9'} marginLeft='10px'>
-                            <CardHeader>
+                        <Card width='270px' height='350px' bg={'#F9F9F9'} marginLeft='10px' className='cardsProyecto'>
+                            <Stack>
+
                                 <Image src={Fotaki} height='150px' />
                                 <Heading size='md'>Promove torpedo deluxe</Heading>
-                            </CardHeader>
+                            
+                            </Stack>
                             <CardBody>
                                 <Text>Video presentacion nuevo modelo de mate deluxe!</Text>
                                 <Button marginTop='4px' as={motion.button} paddingX='65px' whileHover={{ color: '#0C7FB0' }} color={'#F9F9F9'} bg={'#0C7FB0'} variant='solid'>View on github</Button>
@@ -96,6 +107,56 @@ const Service = () => {
                     </Stack>
                 </VStack>
             </Container>
+
+
+            <Stack id='testimonios' width={'full'} margin="0 auto">
+            <VStack   gap="45px" marginLeft="40px">
+                <VStack>
+                    <Text color={'#0C7FB0'} marginTop="20px">Client's feedback</Text>
+                    <HStack>
+                        <Image src={Azul} className='imgabstractaFeed' />
+                    </HStack>
+                    <Stack>
+                        <Heading>Testimonials</Heading>
+                    </Stack>
+                </VStack>
+                <HStack gap="30px" className='stackFeed' marginLeft="45px" margin="0 auto">
+                    <Card as={motion.div} whileHover={{ scale: 1.1, color: '#0C7FB0' }} className='cardsFeedback' bg={'#F9F9F9'} width='300px' height='300px'>
+                        <HStack gap={10}>
+                            <Image className='imgFeed' src={cami} />
+                            <Image src={Comillas} w='100px' className='comillasfeed'/>
+                        </HStack>
+                        <CardBody className='cardsFeedback'>
+                            <Heading size='md'> Camila Moncada</Heading>
+                            <Image src={Five} className='fiveStars' />
+                            <Text>Manuel realiza trabajos muy buenos, todos fueron entregados en tiempo y forma; ademas trabaja muy bien con su equipo, sin duda volveria a contratarlos</Text>
+                        </CardBody>
+                    </Card>
+                    <Card as={motion.div} whileHover={{ scale: 1.1, color: '#0C7FB0' }} className='cardsFeedback' bg={'#F9F9F9'} width='300px' height='300px'>
+                        <HStack >
+                            <Image className='imgFeed' src={alexis} />
+                            <Image src={Comillas} w='100px' />
+                        </HStack>
+                        <CardBody className='cardsFeedback'>
+                            <Heading size='md'>Alexis juncos</Heading>
+                            <Image src={Five} className='fiveStars' />
+                            <Text>Manuel se encargo de la promocion de mis productos materos y se dispararon las ventas!</Text>
+                        </CardBody>
+                    </Card>
+                    <Card as={motion.div} whileHover={{ scale: 1.1, color: '#0C7FB0' }} bg={'#F9F9F9'} className='cardsFeedback' width='300px' height='300px'>
+                        <HStack >
+                            <Image className='imgFeed' src={marcos} />
+                            <Image src={Comillas} w='100px' />
+                        </HStack>
+                        <CardBody className='cardsFeedback'>
+                            <Heading size='md'>Marcos Servezinski </Heading>
+                            <Image src={Five} className='fiveStars' />
+                            <Text>Los trabajos que Manuel ha hecho son muy ingenios, las ideas que yo le dije el las tranformo tal cual yo me las imaginaga!</Text>
+                        </CardBody>
+                    </Card>
+                </HStack>
+            </VStack>
+        </Stack>
         </>
     )
 }
