999157bdfed2a91b861f8eae0e5f8f9dd8028d4f
Manunyville <125371151+Manunyville@users.noreply.github.com>
2023-04-20 14:26:47 -0300

    creacion de componente servicios con sus cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import './App.css'
 import Hero from './componentes/Hero/hero'
 import About from './componentes/about/about'
+import Service from './componentes/servicio/servicio'
 
 function App() {
  
@@ -11,6 +12,7 @@ function App() {
     <div>
    <Hero/>
    <About/>
+   <Service/>
     </div>
   )
 }
---
+++ src/componentes/servicio/servicio.jsx
@@ -0,0 +1,53 @@
+import './servicio.css'
+import React from 'react'
+import { Stack, HStack, VStack, Flex, Button, ButtonGroup, Text, Container, Heading, Image, Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
+import { motion } from "framer-motion"
+import Web from '../../assets/img/codigoweb.png'
+import diseño from '../../assets/img/Diseñodepagina.png'
+import Diseñoweb from '../../assets/img/desarrolloweb.png'
+
+
+const Service = () => {
+
+    return (
+
+        <Container width="full" id='misServicios'>
+            <VStack gap="15px">
+                <VStack marginTop="20px">
+                    <Text color={'#0C7FB0'}>Services I provide</Text>
+                    <Heading>My Services</Heading>
+                </VStack>
+                <HStack height="300px" width="1200px" justify={"space-evenly"}>
+                    <Card className='cardServ' bg={'#F9F9F9'} height="150px" width="220px">
+                        <Stack>
+                            <Image className='webImg' src={Web} />
+                        </Stack>
+                        <CardBody>
+                            <Heading size='md'> Website developement </Heading>
+                            <Text>Yo transformo ideas complejas en paginas web sencillas de utilizar.</Text>
+                        </CardBody>
+                    </Card>
+                    <Card className='cardServ' bg={'#F9F9F9'} height="150px" width="220px">
+                        <Stack>
+                            <Image className='webImg' src={Diseñoweb} />
+                        </Stack>
+                        <CardBody>
+                            <Heading size='md'>Website design</Heading>
+                            <Text>Realizo los de las paginas de acuerdo a lo que el cliente desee. </Text>
+                        </CardBody>
+                    </Card>
+                    <Card bg={'#F9F9F9'} className='cardServ' height="150px" width="220px" >
+                        <Stack>
+                            <Image className='webImg' src={diseño} />
+                        </Stack>
+                        <CardBody>
+                            <Heading size='md'>Web manteinance </Heading>
+                            <Text>Realizo actividades de mantenimiento para optimizar el funcionamiento.</Text>
+                        </CardBody>
+                    </Card>
+                </HStack>
+            </VStack>
+        </Container>
+    )
+}
+export default Service
